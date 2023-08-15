fc564a3d82c1cca27c92afe8925c698b5ff0c160
falconlee236 <falconlee236@cau.ac.kr>
2023-08-15 21:56:45 +0900

    Update MainActivity.kt: Add padding in Text composable

--- ComposeArticle/app/src/main/java/com/example/composearticle/MainActivity.kt
+++ ComposeArticle/app/src/main/java/com/example/composearticle/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -14,6 +15,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composearticle.ui.theme.ComposeArticleTheme
 
 class MainActivity : ComponentActivity() {
@@ -43,15 +46,16 @@ fun ArticleView(name: String, modifier: Modifier = Modifier) {
         )
         Text(
             text = stringResource(id = R.string.article_title),
-            modifier = modifier
+            fontSize = 24.sp,
+            modifier = modifier.padding(16.dp)
         )
         Text(
             text = stringResource(id = R.string.first_para),
-            modifier = modifier
+            modifier = modifier.padding(16.dp, 0.dp)
         )
         Text(
             text = stringResource(id = R.string.second_para),
-            modifier = modifier
+            modifier = modifier.padding(16.dp)
         )
     }
 }
